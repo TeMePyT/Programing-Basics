6389c6a976d283b4900e3d1c2654ac5c8fe878b4
TeMePyT <36801088+TeMePyT@users.noreply.github.com>
2018-02-24 23:01:39 +0200

    Add files via upload

---
+++ Advanced-Loops/1toN_With_Step_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1toN_With_Step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n= ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The numbers are:");
+            for (int i = 1; i <=n; i+=3)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
