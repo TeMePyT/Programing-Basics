5bf24ea4d5bd6af4a77320dcccedb01ee2d8a8e6
TeMePyT <36801088+TeMePyT@users.noreply.github.com>
2018-02-24 23:49:31 +0200

    Add files via upload

---
+++ Exam_05_November_2017/Lutenitsa/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lutenitsa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tomatoes = double.Parse(Console.ReadLine());
+            int cassettes = int.Parse(Console.ReadLine());
+            int jarsPerCassette = int.Parse(Console.ReadLine());
+
+            double lutenitsaKg = tomatoes / 5;
+            double jarsFilled = lutenitsaKg / 0.535;
+            double cassettesFilled = jarsFilled / jarsPerCassette;
+
+            Console.WriteLine($"Total lutenica: {Math.Floor(lutenitsaKg)} kilograms.");
+
+            if (cassettesFilled>cassettes)
+            {
+                Console.WriteLine($"{Math.Floor(cassettesFilled-cassettes)} boxes left.");
+                Console.WriteLine($"{Math.Floor(jarsFilled-(cassettes*jarsPerCassette))} jars left.");
+            }
+            else
+            {
+                Console.WriteLine($"{Math.Floor(cassettes-cassettesFilled)} more boxes needed.");
+                Console.WriteLine($"{Math.Floor(Math.Abs(jarsFilled - (cassettes * jarsPerCassette)))} more jars needed.");
+            }
+            
+        }
+    }
+}
---
+++ Exam_05_November_2017/Wire_Net/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wire_Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wightYard = int.Parse(Console.ReadLine());
+            int heightYard = int.Parse(Console.ReadLine());
+            double netHeight = double.Parse(Console.ReadLine());
+            double netPrice = double.Parse(Console.ReadLine());
+            double netWeightPerSqMeter = double.Parse(Console.ReadLine());
+
+            int yardPerimeter = 2 * wightYard + 2 * heightYard;
+            double totalNetPrice = yardPerimeter * netPrice;
+            double totalNetArea = yardPerimeter * netHeight;
+            double totalNetWeight = totalNetArea * netWeightPerSqMeter;
+
+            Console.WriteLine(yardPerimeter);
+            Console.WriteLine($"{totalNetPrice:F2}");
+            Console.WriteLine($"{totalNetWeight:F3}");
+        }
+    }
+}
