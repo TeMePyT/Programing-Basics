ca1db91f4fef1f208666efa5aaa09939c8c9c3a8
TeMePyT <36801088+TeMePyT@users.noreply.github.com>
2018-02-24 23:03:41 +0200

    Add files via upload

---
+++ Advanced-Loops/Coins/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = (int)(decimal.Parse(Console.ReadLine()) * 100);
+            IList<int> coinTypes = new List<int> { 200, 100, 50, 20, 10, 5, 2, 1 };
+            int coinCount = 0;
+
+            foreach (int coinType in coinTypes)
+            {
+                if (input >= coinType)
+                {
+                    coinCount += input / coinType;
+                    input -= (input / coinType) * coinType;
+                }
+            }
+
+            Console.WriteLine(coinCount);
+        }
+    }
+}
---
+++ Advanced-Loops/DeleneBezOstatyk/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleneBezOstatyk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double p1 = 0;
+            double p2 = 0;
+            double p3 = 0;
+            int p = 0;
+            while(p < n)
+            {
+                int number = int.Parse(Console.ReadLine());
+                p++;
+                if (number % 2 == 0)
+                {
+                    p1++;
+                }
+                if (number % 3 == 0)
+                {
+                    p2++;
+                }
+                if (number % 4 == 0)
+                {
+                    p3++;
+                }
+            } 
+            Console.WriteLine("{0:f2}%", p1/n*100);
+            Console.WriteLine("{0:F2}%", p2/n*100);
+            Console.WriteLine("{0:F2}%", p3/n*100);
+        }
+    }
+}
