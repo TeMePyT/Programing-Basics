8d9d71317ee6f4d2e7bcaa3e765a0c293af92075
TeMePyT <36801088+TeMePyT@users.noreply.github.com>
2018-02-24 23:02:21 +0200

    Add files via upload

---
+++ Advanced-Loops/Check_Prime/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n < 2)
+            {
+                Console.WriteLine("Not prime");
+            }
+            bool isPrime = true;
+
+            for (int i = 2; i <= n - 1; i++)
+            {
+                if (n % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            string result = isPrime ? "Prime" : "Not Prime";
+            Console.WriteLine(result);
+        }
+    }
+}
