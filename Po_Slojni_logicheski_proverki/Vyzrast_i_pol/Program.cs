872ab3c4ec553f4aa9def80080cdebf632fc10e6
TeMePyT <36801088+TeMePyT@users.noreply.github.com>
2018-02-25 00:31:22 +0200

    Add files via upload

---
+++ Po_Slojni_logicheski_proverki/Vyzrast_i_pol/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vyzrast_i_pol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var age = double.Parse(Console.ReadLine());
+            var sex = Console.ReadLine();
+
+            if(sex=="f")
+            {
+                if (age<16)
+                {
+                    Console.WriteLine("Miss");
+                }
+                else
+                {
+                    Console.WriteLine("Ms.");
+                }
+            }
+            else if (sex=="m")
+            {
+                if (age<16)
+                {
+                    Console.WriteLine("Master");
+                }
+                else
+                {
+                    Console.WriteLine("Mr.");
+                }
+            }
+        }
+    }
+}
---
+++ Po_Slojni_logicheski_proverki/voleyball/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace voleyball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string year = Console.ReadLine();
+            double p = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            double total = (48 - h) * 3 / 4 + (p * 2 / 3) + h;
+
+            if (year == "leap")
+            {
+                Console.WriteLine(Math.Floor(total + 0.15 * total));
+            }
+            else if (year == "normal")
+            {
+                Console.WriteLine(Math.Floor(total));
+            }
+        }
+    }
+}
