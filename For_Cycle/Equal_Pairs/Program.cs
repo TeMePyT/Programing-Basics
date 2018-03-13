3e7030b06c7a082e5eef210820a071d7d35ca0ee
Marin Stankov <36801088+TeMePyT@users.noreply.github.com>
2018-03-13 19:25:01 +0200

    Add files via upload

---
+++ For_Cycle/Equal_Pairs/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equal_Pairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int maxDiff = 0;
+            int prevSum = 0;
+          
+            for (int i = 0; i < n * 2; i += 2)
+            {
+                int number1 = int.Parse(Console.ReadLine());
+                int number2 = int.Parse(Console.ReadLine());
+                int currentSum = number1 + number2;
+                if (i == 0)
+                {
+                    prevSum = currentSum;
+                    sum = currentSum;
+                    
+                }
+                else
+                {
+                    if (prevSum!=currentSum)
+                    {
+                        int currentMaxDiff = Math.Abs(prevSum-currentSum);
+                        if(currentMaxDiff>maxDiff)
+                        {
+                            maxDiff = currentMaxDiff;
+                        }
+
+                    }
+                  
+                }
+                prevSum = currentSum;
+            }
+            
+
+            if (maxDiff == 0)
+            {
+                Console.WriteLine($"Yes, value={sum}");
+            }
+            else
+            {
+                Console.WriteLine($"No, maxdiff={maxDiff}");
+            }
+        }
+    }
+}
---
+++ For_Cycle/Odd_Even_Position/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odd_Even_Position
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double evenSum = 0;
+            double oddSum = 0;
+            double oddMin = double.MaxValue;
+            double oddMax = double.MinValue;
+            double evenMin = double.MaxValue;
+            double evenMax = double.MinValue;
+
+            for (int i = 1; i <= n; i++)
+            {
+                double nextNumber = double.Parse(Console.ReadLine());
+                
+                
+                if(i%2==0)
+                {
+                    evenSum += nextNumber;
+                    if (n == 2)
+                    {
+                        evenMin = nextNumber;
+                        evenMax = nextNumber;
+                    }
+                     if (nextNumber>evenMax)
+                    {
+                        evenMax = nextNumber;
+                    }
+                    if(nextNumber<evenMin )
+                    {
+                        evenMin = nextNumber;
+                    }
+                   
+                }
+                else
+                {
+                    oddSum += nextNumber;
+                    if(n==1||n==2)
+                    {
+                        oddMin = nextNumber;
+                        oddMax = nextNumber;
+                    }
+                   
+                    if(nextNumber>oddMax)
+                    {
+                        oddMax = nextNumber;
+                    }
+                    if(nextNumber<oddMin)
+                    {
+                        oddMin = nextNumber;
+                    }
+                    
+                    
+                }
+            }
+
+            Console.WriteLine($"OddSum={oddSum},");
+           
+            if(oddSum>0 || oddSum<0)
+            {
+                Console.WriteLine($"OddMin={oddMin},");
+                Console.WriteLine($"OddMax={oddMax},");
+            }
+            else
+            {
+                Console.WriteLine("OddMin=No,");
+                Console.WriteLine("OddMax=No");
+            }
+
+            Console.WriteLine($"EvenSum={evenSum},");
+            if (evenSum>0 || evenSum<0)
+            {
+                Console.WriteLine($"EvenMin={evenMin},");
+                Console.WriteLine($"EvenMax={evenMax}" );
+            }
+            else
+            {
+                Console.WriteLine("EvenMin=No,");
+                Console.WriteLine("EvenMax=No");
+            }
+            
+           
+        }
+    }
+}
