4f2c9e75c24fbbc6870fd9ecdb2f310491dcfb1e
Marin Stankov <36801088+TeMePyT@users.noreply.github.com>
2018-03-14 23:17:49 +0200

    Add files via upload

--- Programming Basics Online Exam - 10 and 11 March 2018/ASCII_Combinations_2/Program.cs
+++ Programming Basics Online Exam - 10 and 11 March 2018/ASCII_Combinations_2/Program.cs
@@ -10,6 +10,64 @@ namespace ASCII_Combinations_2
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+
+            int numberSum = 0;
+            int capitalSum = 0;
+            int smallSum = 0;
+            int restSum = 0;
+
+            string numbers = "";
+            string smallLetters = "";
+            string capitalLetters = "";
+            string rest = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                char nextChar = Convert.ToChar(Console.ReadLine());
+                if (nextChar >= '0' && nextChar <= '9')
+                {
+                    numberSum += nextChar;
+                    numbers += nextChar;
+                }
+
+                else if (nextChar >= 'a' && nextChar <= 'z')
+                {
+                    smallLetters += nextChar;
+                    smallSum += nextChar;
+                }
+                else if (nextChar >= 'A' && nextChar <= 'Z')
+                {
+                    capitalSum += nextChar;
+                    capitalLetters += nextChar;
+                }
+                else
+                {
+                    restSum += nextChar;
+                    rest += nextChar;
+                }
+            }
+            if (numberSum >= smallSum && numberSum >= capitalSum && numberSum >= restSum)
+            {
+                Console.WriteLine($"Biggest ASCII sum is:{numberSum}");
+                Console.WriteLine($"Combination of characters is:{numbers}");
+            }
+            else if (capitalSum > numberSum && capitalSum >= smallSum && capitalSum >= restSum)
+            {
+                Console.WriteLine($"Biggest ASCII sum is:{capitalSum}");
+                Console.WriteLine($"Combination of characters is:{capitalLetters}");
+            }
+            else if (smallSum > numberSum && smallSum > capitalSum && smallSum >= restSum)
+            {
+                Console.WriteLine($"Biggest ASCII sum is:{smallSum}");
+                Console.WriteLine($"Combination of characters is:{smallLetters}");
+            }
+            else
+            {
+                Console.WriteLine($"Biggest ASCII sum is:{restSum}");
+                Console.WriteLine($"Combination of characters is:{rest}");
+            }
         }
     }
+
 }
